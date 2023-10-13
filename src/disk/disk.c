e205bbdf9ceeca6d975a1578b89123a24b59bc17
wh0cares1 <95743065+wh0cares1@users.noreply.github.com>
2023-10-13 15:07:39 +0700

    Improving Disk Driver

--- src/disk/disk.c
+++ src/disk/disk.c
@@ -1,4 +1,11 @@
 #include "io/io.h"
+#include "disk.h"
+#include "config.h"
+#include "memory/memory.h"
+#include "status.h"
+
+struct disk disk;
+
 int disk_read_sector(int lba, int total, void* buf)
 {
     outb(0x1F6, (lba >> 24) | 0xE0);
@@ -26,4 +33,28 @@ int disk_read_sector(int lba, int total, void* buf)
         }
     }
     return 0;
+}
+
+void disk_search_and_init()
+{
+    memset(&disk, 0, sizeof(disk));
+    disk.type = DISK_TYPE_REAL;
+    disk.sector_size = SECTOR_SIZE;
+}
+
+struct disk* disk_get(int index)
+{
+    if (index != 0)
+        return 0;
+
+    return &disk;
+}
+
+int disk_read_block(struct disk* idisk, unsigned int lba, int total, void* buf)
+{
+    if (idisk != &disk)
+    {
+        return -EIO;
+    }
+    return disk_read_sector(lba, total, buf);
 }
--- src/kernel.c
+++ src/kernel.c
@@ -69,6 +69,9 @@ void kernel_main(){
     // Initialize the heap
     kheap_init();
 
+    // Search and initialize the disk
+    disk_search_and_init();
+
     // Initialize the Interrupt Descriptor Table
     idt_init();
 
@@ -80,9 +83,6 @@ void kernel_main(){
 
     // Enable paging
     enable_paging();
-
-    char buf[512];
-    disk_read_sector(0, 1, buf);
 
     // Enable the system interrupts
     enable_interrupts();
